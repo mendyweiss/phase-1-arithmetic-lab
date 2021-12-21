6c4587110dc8d7b41233c4a72789becaee111593
Mendy <mendyw94@gmail.com>
2021-12-21 11:10:51 -0500

    commit tree bb0d7cb71be10cfeb0bc64f1999bfbd54af67e17

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+random;//     Math.random() * 10;
+// }
+
+// random();
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const myArray = [5, 7, 20, 4, 15];
+
+const max = Math.max(...myArray);
+
+
